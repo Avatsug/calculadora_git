ee0bf43d59aa2476afb5face1619b8b6a006a4e7
Avatsug <gustavoaparicio2003@gmail.com>
2023-09-10 15:45:04 -0500

    Clase para multiplicar

--- main.py
+++ main.py
@@ -19,3 +19,12 @@ class resta:
 
     def solucion(self):
         return self.numero_1 - self.numero_2
+    
+#Clase para multiplicar
+class multiplicacion:
+    def __init__(self, numero_1, numero_2):
+        self.numero_1 = numero_1
+        self.numero_2 = numero_2
+
+    def solucion(self):
+        return self.numero_1 * self.numero_2
