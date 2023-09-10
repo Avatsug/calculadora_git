a9c852ff9d55ba4b152d968448b9e5e10bc07c16
Avatsug <gustavoaparicio2003@gmail.com>
2023-09-10 15:48:20 -0500

    Clase para elevar/potenciar

--- main.py
+++ main.py
@@ -37,3 +37,12 @@ class division:
 
     def solucion(self):
         return self.numero_1 // self.numero_2
+    
+# Clase para elevar/potenciar
+class potenciacion:
+    def __init__(self, numero_1, numero_2):
+        self.numero_1 = numero_1
+        self.numero_2 = numero_2
+
+    def solucion(self):
+        return self.numero_1 ** self.numero_2
